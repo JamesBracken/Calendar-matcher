b08b4abc9b55de364a09cebf674a5c4c32cfd136
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-08-23 22:06:40 +0100

    Add: Employee entity

--- src/main/java/com/example/calendar/Employee/entities/Employee.java
+++ src/main/java/com/example/calendar/Employee/entities/Employee.java
@@ -1,2 +1,63 @@
 package com.example.calendar.Employee.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    //May want to expand to include Dates
+
+    //Name
+    private String name;
+
+    //List of meetings
+    private List<List<String>> meetings;
+
+    //Daily bounds
+    private List<String> bounds;
+
+    public Employee() {
+    }
+
+    public List<String> getBounds() {
+        return bounds;
+    }
+
+    public void setBounds(List<String> bounds) {
+        this.bounds = bounds;
+    }
+
+    public Employee(String name, ArrayList<List<String>> meetings, List<String> bounds) {
+        this.name = name;
+        this.meetings = meetings;
+        this.bounds = bounds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<List<String>> getMeetings() {
+        return meetings;
+    }
+
+    public void setMeetings(List<List<String>> meetings) {
+        this.meetings = meetings;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", meetings=" + meetings +
+                '}';
+    }
+}
